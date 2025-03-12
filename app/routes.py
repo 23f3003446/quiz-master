a1cb31cdcdaf0e61a6dc108c8aa9da01f2da2c85
23f3003446 <23f3003446@ds.study.iitm.ac.in>
2025-03-12 08:12:13 +0530

    created database schema for users, subjects, quizzes and scores. implemented user registration and admin login functionality.

---
+++ app/routes.py
@@ -0,0 +1,36 @@
+from app import create_app, db
+from flask import render_template, redirect, flash, url_for
+from app.forms import RegisterForm, LoginForm
+from app.models import User, Subject, Chapter, Quiz, Question, Score
+
+app = create_app()
+
+@app.cli.command('db-create')
+def create_db():
+    db.create_all()
+    print("DB Created")
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        flash('Account Registration successful!')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login Successful!')
+        return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
